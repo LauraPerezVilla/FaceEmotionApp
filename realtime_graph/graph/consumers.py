7edca09407b5d7b576751d85a8af4c1f1fde9cde
Juan Esteban Cortés Gómez <juancortesg1204@gmail.com>
2025-03-16 20:06:13 -0500

    Merge code union with main (#1)
    * Added code union
    
    * Union of FaceEmotion code and Django
    
    * Requirements
    
    ---------
    
    Co-authored-by: Juan Cortés <juan.cortes@slash-digital.io>

--- realtime_graph/graph/consumers.py
+++ realtime_graph/graph/consumers.py
@@ -1,14 +1,35 @@
 import json
-from random import randint
-from asyncio import sleep
 from channels.generic.websocket import AsyncWebsocketConsumer
+from .model import FaceEmotionModel, NpEncoder
+import cv2
+import numpy as np
 
+classes = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
 
 class GraphConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
+    
+    async def receive(self, bytes_data):
+        # Convertir los bytes a un numpy array (como uint8)
+        np_array = np.frombuffer(bytes_data, dtype=np.uint8)
+        frame = np_array.reshape((240, 320, 3))  # Ajustar dimensiones
+        frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGBA2BGR)  # Convertir a BGR
+        
+        # Hacemos la predicción
+        face_model = FaceEmotionModel()
+        locs, preds = face_model.predict_emotion(frame_bgr)
+        
+        # Depuración
+        for (box, pred) in zip(locs, preds):
+            label = "{}: {:.0f}%".format(classes[np.argmax(pred)], max(pred) * 100)
+            (Xi, Yi, Xf, Yf) = box
+            cv2.rectangle(frame_bgr, (Xi, Yi-40), (Xf, Yi), (255, 0, 0), -1)
+            cv2.putText(frame_bgr, label, (Xi+5, Yi-15), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+            cv2.rectangle(frame_bgr, (Xi, Yi), (Xf, Yf), (255, 0, 0), 3)
+        cv2.imshow("Frame predecido", frame_bgr)
+        cv2.waitKey(1)  # Mantiene la ventana activa
 
+        # Enviamos la predicción al cliente
+        await self.send(text_data=json.dumps({'loc': locs, 'preds': preds}, cls=NpEncoder))
 
-        for i in range(1000):
-            await self.send(json.dumps({'value': randint(-20, 20)}))
-            await sleep(1)
--- realtime_graph/static/main.js
+++ realtime_graph/static/main.js
@@ -1,33 +1,119 @@
-const ctx = document.getElementById('myChart');
+const ctx = document.getElementById("myChart");
+let video = document.getElementById("videoInput");
+
+// Checkeamos si el navegador soporta mediaDevices
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Pedimos acceso a la cámara del usuario
+    navigator.mediaDevices.getUserMedia({ video: true })
+    .then((stream) => {
+        // Set the video source to the camera stream
+        video.srcObject = stream;
+    })
+    .catch((error) => {
+        console.error("Error accediendo a la cámara: ", error);
+    });
+} else {
+    console.log("getUserMedia no es soportado por el navegador.");
+}
+
 
 var graphData = {
-    type: 'line',
-    data: {
-      labels: ['jan', 'feb', 'mar', 'apr', 'may', 'june'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+  type: "bar",
+  data: {
+    labels: ["angry", "disgust", "fear", "happy", "neutral", "sad", "surprise"],
+    datasets: [
+      {
+        label: "Emotion predict",
+        data: [0, 0, 0, 0, 0, 0, 0],
         backgroundColor: [
-            'rgba(73,198,230,0.5)',
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(201, 203, 207, 0.2)",
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
         ],
         borderWidth: 1,
-        tension:0.5,
-        fill:true
-      }]
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
     },
-    options: {}
-  }
+  },
+};
 var myChart = new Chart(ctx, graphData);
-var socket = new WebSocket('ws://127.0.0.1:8000/ws/graph/');
 
-socket.onmessage = function(e){
+var socket = new WebSocket(
+  "ws://127.0.0.1:8010/ws/graph/"
+);
+// Configurar WebSocket para manejar binarios
+socket.binaryType = "arraybuffer";
+
+socket.onopen = function (e) {
+    processVideo()
+}
+
+socket.onmessage = function (e) {
     var djangoData = JSON.parse(e.data);
     console.log(djangoData);
 
+    // Actualizar el gráfico
     var newGraphData = graphData.data.datasets[0].data;
-    newGraphData.shift();
-    newGraphData.push(djangoData.value);
+    newGraphData = djangoData.preds[0];
 
     graphData.data.datasets[0].data = newGraphData;
     myChart.update();
+};
+
+socket.onclose = function() {
+    console.log("WebSocket connection closed");
 }
+
+function processVideo() {
+    let cap = new cv.VideoCapture(video);
+    let frame = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+
+    function sendFrame() {
+        if (socket.readyState !== WebSocket.OPEN) return;
+
+         // Capturar el frame
+        cap.read(frame);
+
+        // Reducir la resolución del frame para enviar menos datos
+        let reducedFrame = new cv.Mat();
+        cv.resize(frame, reducedFrame, new cv.Size(320, 240)); 
+
+        let data = reducedFrame.data; // `Uint8Array` en formato [R, G, B, A, R, G, B, A ...]
+
+        // Convertir RGBA (4 canales) a RGB (3 canales)
+        let rgbArray = new Uint8Array(320 * 240 * 3);
+        let index = 0;
+        for (let i = 0; i < data.length; i += 4) {
+            rgbArray[index] = data[i];     // R
+            rgbArray[index + 1] = data[i + 1]; // G
+            rgbArray[index + 2] = data[i + 2]; // B
+            index += 3;
+        }
+
+        socket.send(rgbArray.buffer); // Enviar datos como binario
+
+        setTimeout(sendFrame, 1000); // Enviamos el frame al servidor cada 1s 
+    }
+
+    sendFrame();
+}
+
