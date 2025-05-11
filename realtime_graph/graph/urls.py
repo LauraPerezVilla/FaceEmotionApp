99351b1314281b46798f4017318d9c84efcef2af
Juan Cortés <juan.cortes@slash-digital.io>
2025-05-11 13:39:23 -0500

    Fix urls

--- realtime_graph/graph/urls.py
+++ realtime_graph/graph/urls.py
@@ -4,8 +4,8 @@ from .views import index, history, real_time, redirect_view
 
 
 urlpatterns= [
-    path ('', index),
+    path('', index),
     path('redirect_view/', redirect_view, name='redirect_view'),
-    path ('real_time/<str:session_id>/', real_time, name='real_time'),
+    path('real_time/<str:session_id>/', real_time, name='real_time'),
     path('history/<str:session_id>/', history, name='history'),
 ]
